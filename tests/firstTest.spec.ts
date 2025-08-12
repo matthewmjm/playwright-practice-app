3c79ca145a1b85ecbfb2d83e6fafecb2e91cefa4
matthewmjm <matthewmjm@yahoo.com>
2025-08-12 17:48:30 -0600

    wrote second test with suites

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,7 +1,29 @@
 import {test} from '@playwright/test'
 
-test('the first test', async ({page}) => {
+test.beforeEach(async ({page}) => {
   await page.goto('http://localhost:4200')
-  await page.getByText('Forms').click()
-  await page.getByText('Form Layout').click()
+})
+
+
+test.describe('suite1', () =>{
+  test.beforeEach(async ({page}) => {
+    await page.getByText('Forms').click()
+    })
+  
+  test('navigate to form layouts', async ({page}) => {
+    await page.getByText('Form Layout').click()
+  })
+  
+  test('navigate to the datepicker page', async ({page}) => {
+    await page.getByText('Datepicker').click()
+  })
+})
+
+test.describe('suite2', () =>{
+
+  test('navigate to charts layouts', async ({page}) => {
+    await page.getByRole('link',{ name: 'Charts', exact: true}).click()
+    await page.getByRole('link',{ name: 'Echarts', exact: true}).click()
+  })
+  
 })
