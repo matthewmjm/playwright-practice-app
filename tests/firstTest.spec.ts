83684a46a76209cddf22080028a7eba63bd46178
matthewmjm <matthewmjm@yahoo.com>
2025-08-14 11:31:55 -0600

    User-facing locators

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -34,3 +34,19 @@ test('Locator syntax rules', async ({page}) => {
   // By Exact text match
   page.locator(':text("Using the Grid")')
 })
+
+test('User facing locators', async ({page}) => {
+  await page.getByRole('textbox', {name: "Email"}).first().click()
+  await page.getByRole('button', {name: "Sign in"}).first().click()
+  
+  await page.getByLabel('Email').first().click()
+  
+  await page.getByPlaceholder('Jane Doe').click()
+  
+  await page.getByText('Using the Grid').click()
+
+  await page.getByTestId('Signin').click()
+  
+  await page.getByTitle('IoT Dashboard').click()
+
+})
